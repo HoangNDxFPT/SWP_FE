833db12eddf4055b62a9e00bddf8b1d27eca404c
HoangNDxFPT <hoangnguyenduc674@gmail.com>
2025-08-05 20:54:51 +0700

    5/8

--- FE/src/member/components/Footer.jsx
+++ FE/src/member/components/Footer.jsx
@@ -6,7 +6,7 @@ function Footer() {
             <div className="max-w-4xl mx-auto px-4">
                 <hr className="mb-4 border-gray-300" />
                 <div className="text-center text-gray-700 text-sm">
-                    © 2006–{new Date().getFullYear()} Foundation for a Drug-Free World. All Rights Reserved. The Foundation for a Drug-Free World Logo is a trademark owned by the Foundation for a Drug-Free World.{' '}
+                    © 2025{new Date().getFullYear()} Foundation for a Drug-Free World. All Rights Reserved. The Foundation for a Drug-Free World Logo is a trademark owned by the Foundation for a Drug-Free World.{' '}
    
                 </div>
             </div>
